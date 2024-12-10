a9cccb9e1edf977edebae487bca16da068eace60
Darkfllame <ntmconar5@gmail.com>
2024-12-10 17:12:02 +0100

    modified the demo to use Zwl.platform.W32 instead of custom-built solution for console output codepage

--- src/main.zig
+++ src/main.zig
@@ -11,14 +11,11 @@ const TRIANGLE_VERTICES = [_]f32{
     0,    0.5,  0, 0, 1,
 };
 
-// pub const panic = Zwl.MBpanic;
-
 pub fn main() !void {
     if (builtin.os.tag == .windows) {
         // manually set console output mode for windows, because zig doesn't
-        _ = (opaque {
-            pub extern "Kernel32" fn SetConsoleOutputCP(wCodePageID: u32) i32;
-        }).SetConsoleOutputCP(65001);
+        const W32 = Zwl.platform.W32;
+        _ = W32.SetConsoleOutputCP(W32.CP_UTF8);
     } else if (builtin.os.tag == .linux) {
         return linuxMain();
     }
